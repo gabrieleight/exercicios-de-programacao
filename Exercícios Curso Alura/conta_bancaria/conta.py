36ee486d9a518c4dda4b587730ed7b990531c73e
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-06-10 16:58:28 -0300

    Commit changes - 16:58 Brazil - 06/10/2022

--- Exercícios Curso Alura/conta_bancaria/conta.py
+++ Exercícios Curso Alura/conta_bancaria/conta.py
@@ -1,29 +1,30 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo objeto... Localização: {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
     def extrato(self):
-        print(f"O saldo da conta do titular {self.titular} é de R$ {self.saldo:.2f}")
+        print(f"O saldo da conta do titular {self.__titular} é de R$ {self.__saldo:.2f}")
     
-    def deposita(self, valor):
-        self.saldo += valor
+    def depositar(self, valor):
+        self.__saldo += valor
         
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+        
+    def transfere(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
         
 
 conta1 = Conta(496256, 'Gabriel Gilberto', 660, 2500)
 conta2 = Conta(496854, 'Emmanuel Nicolas', 5500, 10000)
 
-print(conta1.saldo)
+print(conta1._Conta__saldo)
+print(conta2._Conta__saldo)
+conta2.transfere(10, conta1)
 conta1.extrato()
-print(conta2.saldo)
-conta2.extrato()
-conta1.deposita(5000)
-conta1.extrato()
-conta2.sacar(5000)
 conta2.extrato()
