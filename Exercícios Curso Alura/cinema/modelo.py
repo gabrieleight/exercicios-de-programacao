6c7860d7a56e2fb5c39c1f53b31e53c09b5ca563
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-07-14 15:59:04 -0300

    Commit changes - 15:59 Brazil - 07/14/2022

--- Exercícios Curso Alura/cinema/modelo.py
+++ Exercícios Curso Alura/cinema/modelo.py
@@ -50,8 +50,7 @@ class Playlist():
     def listagem(self):
         return self._programas
     
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -73,7 +72,7 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho da playlist: {len(playlist_fim_de_semana.listagem)}')
-
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
+ 
 for programa in playlist_fim_de_semana:
     print(programa)
